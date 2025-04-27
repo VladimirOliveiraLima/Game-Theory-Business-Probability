5e9438a5dfc7916d095db590018a8ffcb016c548
Vladimir Oliveira Lima <79415774+VladimirOliveiraLima@users.noreply.github.com>
2025-04-27 20:32:42 -0300

    Update 11. teststest_payoff_matrix.py

--- 11. teststest_payoff_matrix.py
+++ 11. teststest_payoff_matrix.py
@@ -1,6 +1,6 @@
 from src.montecarlo_game.payoff_matrix import get_estratégia_juridica_matrix
 
 def test_matrix_structure():
-    matrix = get_dilema_prisioneiro_matrix()
+    matrix = get_estratégia_juridica_matrix()
     assert "C" in matrix and "D" in matrix
     assert isinstance(matrix["C"]["C"], tuple)
