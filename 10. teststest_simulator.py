f2c3b6279526f508880ec89ff6d64b312c2e3e5f
Vladimir Oliveira Lima <79415774+VladimirOliveiraLima@users.noreply.github.com>
2025-04-27 20:32:27 -0300

    Update 10. teststest_simulator.py

--- 10. teststest_simulator.py
+++ 10. teststest_simulator.py
@@ -2,7 +2,7 @@ from src.montecarlo_game.simulator import GameSimulator
 from src.montecarlo_game.payoff_matrix import get_estratégia_juridica_matrix
 
 def test_play_returns_dict():
-    matrix = get_dilema_prisioneiro_matrix()
+    matrix = get_estratégia_juridica_matrix()
     sim = GameSimulator(matrix)
     result = sim.play(lambda: "C", lambda: "D")
     assert "payoff_a" in result and "payoff_b" in result
